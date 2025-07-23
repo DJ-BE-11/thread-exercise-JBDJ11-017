53618552bba5797c75e94d45fc9e5209b4fccc04
Yoon-Bin99 <shp06135@naver.com>
2025-07-23 14:46:55 +0900

    Thread step 1-1

--- src/main/java/com/nhnacademy/thread/Counter.java
+++ src/main/java/com/nhnacademy/thread/Counter.java
@@ -21,11 +21,14 @@ public class Counter {
 
     public Counter(long countMaxSize) {
         //TODO#1 countMaxSize < 0 작다면 IllegalArgumentException 예외가 발생 합니다.
-
+        if(countMaxSize<0){
+            throw new IllegalArgumentException();
+        }
         //TODO#2 this.countMaxSize 초기화 합니다.
+        this.countMaxSize = countMaxSize;
 
         //TODO#3 this.count 값을 0으로 초기화 합니다.
-
+        this.count = 0;
     }
 
     public void run() {
@@ -36,7 +39,11 @@ public class Counter {
               Thread.sleep method를 사용하세요.
               https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/lang/Thread.html#sleep(java.time.Duration)
             */
-
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             count++;
 
             /*TODO#5 count 출력
@@ -44,6 +51,8 @@ public class Counter {
                 Thread name : Thread.currentThread().getName();
                 ex) name:my-thread, count:1
              */
+            String name = Thread.currentThread().getName();
+            System.out.print("name : "+name+", count : "+count);
 
         }while (count<countMaxSize);
     }
